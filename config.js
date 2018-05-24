499986bf7c3bf657546b6835957b022ecc8c4966
verge <support@verge.moe>
2018-05-24 19:50:43 +0200

    added a lot of stuff + basic expression parser

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ module.exports = {
         host: "127.0.0.1",
         port: 3306,
         username: "root",
-        password: "jeffw4re223@",
+        password: "",
         database: "verge"
     }
 };
