5bb3e739be64ec974f07d02996c99e459dace66a
verge <support@verge.moe>
2018-05-26 03:21:25 +0200

    now we're cooking with gas

--- public/home/index.php
+++ public/home/index.php
@@ -8,16 +8,35 @@
 <body>
     <div class="container">
         <div class="navbar">
-            <a href="https://verge.moe"><img src="/assets/images/text-black.png"/></a>
-            <a href="https://verge.moe/leaderboard">Leaderboard</a>
-            <a href="https://verge.moe/info">Info</a>            
+            <a href="https://verge.moe"><p class="navName">Verge</p></a>    
+            <a href="https://verge.moe/leaderboard"><p class="navItems">Leaderboard</p></a>
+            <a href="https://verge.moe/info"><p class="navItems">Info</p></a>            
+        </div>
+        <br><br>
+        <div class="welcome">
+            <h1>Welcome to Verge!</h1>
+            <br>
+            <p class="welcomeTxt">Verge is a custom server for <a href="https://osu.ppy.sh">osu!</a></p>
+        </div>
+        <br><br>
+<div class="homeWrapper">
+        <div class="safety">
+            <h3>Is Verge Safe?</h3>
+            <p>If you register and play on osu!Verge you don't have to worry about losing your account on official as you haven't broken any rules. We also encrypt all of your identifying information (HWID, passwords etc.) as we feel this is the correct thing to do.</p> 
         </div>
         <br>
-        <div class="content">
-            <img src="https://verge.moe/assets/images/verge.png" width="600px" height="350px">
-            <h1> Welcome to Verge! </h1>
+        <div class="explained">
+            <h3>How does Verge work?</h3>
+            <p>Once you have <a href="https://verge.moe/register">registered</a> an account you will need to download the switcher and install the certificate and have the switcher change your hosts file.</p> 
         </div>
-    </div>
+        <div class="whatAreYouWaitingFor">
+            <h3>What are you waiting for?</h3>
+            <p>Well, what are you waiting for? Come join the next (maybe best) osu! Custom server!</p> 
+        </div>
+            </div>
+</div>
+
+
 </body>
 
 <script src="https://verge.moe/assets/js/main.js"></script>
