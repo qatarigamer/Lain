476396cee1df25fc19ad6bc3bbcdbaceb1a8e0cd
verge <support@verge.moe>
2018-05-26 03:48:25 +0200

    Fixed /leaderboard and started a basic register page xo

---
+++ public/register/index.php
@@ -0,0 +1,24 @@
+<html lang="en">
+
+<head>
+    <title>Home Page - Verge</title>
+    <link rel="stylesheet" href="https://verge.moe/assets/css/main.css">
+</head>
+
+<body>
+<div class="container">
+<div class="navbar">
+            <a href="https://verge.moe"><p class="navName">Verge</p></a>    
+            <a href="https://verge.moe/leaderboard"><p class="navItems">Leaderboard</p></a>
+            <a href="https://verge.moe/info"><p class="navItems">Info</p></a>            
+        </div>
+</div>
+<br><br>
+<center>
+<div class="register">
+<div class="narrow-content"><h1><i class="fa fa-plus-circle"></i>	Register!</h1>
+</div>
+</center
+</body>
+
+</html
